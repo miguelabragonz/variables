79f00db239b526331fd414ef11bbb740d1920adb
miguelabragonz <55327101+miguelabragonz@users.noreply.github.com>
2019-09-28 11:04:25 -0500

    Initial commit

---
+++ practiceVariables.py
@@ -0,0 +1,20 @@
+'''
+Created on Sep 28, 2019
+
+@author: ITAUser
+'''
+
+a = 4
+
+b = 3.45
+
+c = "string"
+
+a = True
+
+list = [3, 4, 5]
+
+listTwo = ["3", "4", "5"]
+
+list = [True, False, False]
+
